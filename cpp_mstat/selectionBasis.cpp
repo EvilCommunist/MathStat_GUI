b43140fd39aa2184550783ac10d9bd9fb8032d33
EvilCommunist <AT2415216@yandex.ru>
2024-08-06 19:49:01 +0300

    Расширение тестов срр и добавление первой функции

--- cpp_mstat/selectionBasis.cpp
+++ cpp_mstat/selectionBasis.cpp
@@ -1,55 +1,63 @@
 #include "selectionBasis.h"
 
-std::vector<float> getOrdStatistics(std::vector<float> unsortedData)
-{
-	return std::vector<float>();
+std::vector<float> getOrdStatistics(std::vector<float> unsortedData){
+	for (int i = 0; i < unsortedData.size(); i++) {
+		for (int j = i; j < unsortedData.size(); j++) {
+			if (unsortedData[j] < unsortedData[i]) {
+				float temp = unsortedData[j];
+				unsortedData[j] = unsortedData[i];
+				unsortedData[i] = temp;
+			}
+		}
+	}
+	return unsortedData;
 }
 
-std::vector<int> getOrdStatistics(std::vector<int> unsortedData)
-{
-	return std::vector<int>();
+std::vector<int> getOrdStatistics(std::vector<int> unsortedData){
+	for (int i = 0; i < unsortedData.size(); i++) {
+		for (int j = i; j < unsortedData.size() - 1; j++) {
+			if (unsortedData[j] < unsortedData[i]) {
+				int temp = unsortedData[j];
+				unsortedData[j] = unsortedData[i];
+				unsortedData[i] = temp;
+			}
+		}
+	}
+	return unsortedData;
 }
 
 
-float getChosenMid(std::vector<float> ordStatistics)
-{
+float getChosenMid(std::vector<float> ordStatistics){
 	return 0.0f;
 }
 
-float getChosenMid(std::vector<int> ordStatistics)
-{
+float getChosenMid(std::vector<int> ordStatistics){
 	return 1;
 }
 
 
-float getDispersion(std::vector<float> ordStatistics)
-{
+float getDispersion(std::vector<float> ordStatistics){
 	return 0.0f;
 }
 
-float getDispersion(std::vector<int> ordStatistics)
-{
+float getDispersion(std::vector<int> ordStatistics){
 	return 1;
 }
 
 
-float getMedian(std::vector<float> ordStatistics)
-{
+float getMedian(std::vector<float> ordStatistics){
 	return 0.0f;
 }
 
-float getMedian(std::vector<int> ordStatistics)
-{
+float getMedian(std::vector<int> ordStatistics){
 	return 1;
 }
 
 
-float getQuantile(std::vector<float> ordStatistics, short int numberOfQuantile)
-{
+float getQuantile(std::vector<float> ordStatistics, short int numberOfQuantile){
 	return 0.0f;
 }
 
-int getQuantile(std::vector<int> ordStatistics, short int numberOfQuantile)
-{
+int getQuantile(std::vector<int> ordStatistics, short int numberOfQuantile){
 	return 1;
 }
--- cpp_mstat/selection_test/selection_test/selection_test.cpp
+++ cpp_mstat/selection_test/selection_test/selection_test.cpp
@@ -1,10 +1,54 @@
 ﻿#include <iostream>
+#include <fstream>
 #include "../../selectionBasis.h"
 
-int main()
-{
+bool readData1Dim(std::string filepath, std::vector<float>& toFill) {
+    std::ifstream testInput;
+    testInput.open(filepath, testInput.in);
+    if (!testInput.is_open()) { // Проверка на открытие файла
+        std::cout << "Cannot open current file!";
+        return false;
+    }
+
+    float temp{};
+    while (testInput >> temp) {
+        toFill.push_back(temp);
+    }
+    return true;
+}
+bool readData1Dim(std::string filepath, std::vector<int>& toFill) {
+    std::ifstream testInput;
+    testInput.open(filepath, testInput.in);
+    if (!testInput.is_open()) { // Проверка на открытие файла
+        std::cout << "Cannot open current file!";
+        return false;
+    }
+
+    int temp{};
+    while (testInput >> temp) {
+        toFill.push_back(temp);
+    }
+    return true;
+}
+
+void writeVector(std::vector<float> toWrite) {
+    for (int i = 0; i < toWrite.size(); i++) {
+        std::cout << toWrite[i] << " ";
+    }
+}
+void writeVector(std::vector<int> toWrite) {
+    for (int i = 0; i < toWrite.size(); i++) {
+        std::cout << toWrite[i] << " ";
+    }
+}
+
+int main(){
+    std::string filepath = "../../../RStudio_test/r_test_data/test_data_for_1dimension.txt";
     std::vector<float> test;
-    std::cout<<getChosenMid(test);
+    if (!readData1Dim(filepath, test))
+        return 1;
+
+    writeVector(getOrdStatistics(test));
     std::cout << "\nTest successfully completed!";
 }
 
