dfbe3e91d5a21b58817d7250d8f23168c0607913
EvilCommunist <AT2415216@yandex.ru>
2024-09-13 00:44:41 +0300

    Исправление функции проверки распределения.

--- python_interface/handlers_functions/point_estimate/distribution_estimate.py
+++ python_interface/handlers_functions/point_estimate/distribution_estimate.py
@@ -8,8 +8,8 @@ def estimate_distribution(data: list[int] | list[float]) -> tuple[str, float, fl
     res = [ks_test(data, type_) for type_ in standart_distributions]
     p_list = [res_tuple[1] for res_tuple in res]
     d_list = [res_tuple[0] for res_tuple in res]
-    chosen_dist = standart_distributions[p_list.index(max(p_list))]
-    chosen_p = max((p for p in p_list if 0 < p < 1), default=-1)
-    chosen_d = d_list[p_list.index(max(p_list))]
+    chosen_p = max((p for p in p_list if 0 < p <= 1), default=-1)
+    chosen_dist = standart_distributions[p_list.index(chosen_p)]
+    chosen_d = d_list[p_list.index(chosen_p)]
     return ltt([chosen_dist, chosen_p, chosen_d])  # если возвращается -1 - распределение не указано
                                                    # если 0.05 - нулевая гипотеза, что по-сути то же самое
