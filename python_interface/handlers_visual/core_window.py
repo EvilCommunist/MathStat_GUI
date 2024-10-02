4c374d77a051c338c20042599b701706a0ab58f0
EvilCommunist <AT2415216@yandex.ru>
2024-10-02 16:20:05 +0300

    Исправление визуала 1.0

--- python_interface/handlers_visual/core_window.py
+++ python_interface/handlers_visual/core_window.py
@@ -17,32 +17,6 @@ def create_main_buttons(root):
     button_frame.pack(pady=50)
 
     button1 = ttk.Button(
-        button_frame, text="Предметная область", command=lambda: create_buttons_window(root), style='TButton'
-    )
-    button1.pack(pady=10, fill='x', padx=20)
-    button1.focus_set()
-
-    button2 = ttk.Button(
-        button_frame, text="Выход", command=root.quit, style='TButton'
-    )
-    button2.pack(pady=10, fill='x', padx=20)
-
-    root.bind('<Return>', lambda event: button1.invoke() if button1.focus_get() else button2.invoke())
-
-
-def create_buttons_window(root):
-    buttons_window = tk.Toplevel(root)
-    buttons_window.title("Предметная область")
-    buttons_window.attributes('-fullscreen', True)
-    buttons_window.configure(background='white')
-
-    button_frame = ttk.Frame(buttons_window, style='TFrame')
-    button_frame.pack(pady=50)
-
-    style = ttk.Style()
-    style.configure("TButton", background="DodgerBlue3")
-
-    button1 = ttk.Button(
         button_frame, text="Выборка", command=lambda: open_new_window(root), style='TButton'
     )
     button1.pack(pady=10, fill='x', padx=20)
@@ -54,17 +28,23 @@ def create_buttons_window(root):
     button2.pack(pady=10, fill='x', padx=20)
 
     button3 = ttk.Button(
-        button_frame, text="Расчет доверительных интервалов", command=lambda: open_confidence_interval_window(root), style='TButton'
+        button_frame, text="Расчет доверительных интервалов", command=lambda: open_confidence_interval_window(root),
+        style='TButton'
     )
     button3.pack(pady=10, fill='x', padx=20)
 
-    buttons_window.bind('<Return>', lambda event: button1.invoke() if button1.focus_get() else (button2.invoke() if button2.focus_get() else button3.invoke()))
+    button4 = ttk.Button(
+        button_frame, text="Выход", command=root.quit, style='TButton'
+    )
+    button4.pack(pady=10, fill='x', padx=20)
+
+    root.bind('<Return>', lambda event: button1.invoke() if button1.focus_get() else (button2.invoke()
+              if button2.focus_get() else button3.invoke() if button3.focus_get() else button4.invoke()))
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Математическая статистика")
-    root.attributes('-fullscreen', True)
     root.configure(background='white')
 
     root.protocol("WM_DELETE_WINDOW", root.quit)
